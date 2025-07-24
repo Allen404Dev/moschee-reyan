6cc451af9631edd1a3a6b46b32f86b1f67e46de2
codingschool_aabdijanovic <Allen.Abdijanovic@edu.fh-kaernten.ac.at>
2025-07-24 15:57:32 +0200

    start

---
+++ src/routes/__root.tsx
@@ -0,0 +1,18 @@
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <nav className="bg-blue-500 flex flex-row">
+        <div>Über uns</div>
+        <div>Aktivitäten</div>
+        <div>Gottesdienst</div>
+        <div>Moscheekauf</div>
+        <div>Kontakt</div>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  ),
+});
